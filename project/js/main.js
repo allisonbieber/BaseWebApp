1c55826febc914f3162b2a0c034cf9f8700115f9
Allison Bieber <bieber.a@husky.neu.edu>
2020-07-21 20:47:16 -0700

    Changed dog image

--- project/js/main.js
+++ project/js/main.js
@@ -3,9 +3,9 @@ function myFunction() {
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
 
-  $("#demo").html("Thank you for visiting!");
+  $("#demo").html("Thank you for visiting! This is my dog, Junie!");
 
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+  $("#doge-image").append(`<img class="img-circle" src="images/junie.jpeg" height="350"/>`);
 }
 
